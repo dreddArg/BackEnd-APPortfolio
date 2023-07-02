948bbd85e0861f970da5cd92e744cb138d028c7b
Sergio Durelli <75744905+dreddArg@users.noreply.github.com>
2023-07-02 14:30:23 -0300

    new cors configuration

--- src/main/java/com/portfolio/BackEnd/BackEndApplication.java
+++ src/main/java/com/portfolio/BackEnd/BackEndApplication.java
@@ -2,12 +2,28 @@ package com.portfolio.BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndApplication.class, args);
+	}
+        
+        @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+                                        .allowedOrigins("https://frontend-apportfolio.web.app", "http://localhost:4200")
+                                        .allowedMethods("*")
+                                        .allowedHeaders("*");
+			}
+		};
 	}
 
 }
